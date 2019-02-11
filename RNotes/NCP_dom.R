ed9ccc751945c844fab96c13f08f958f62d90c5d
gc5k <chenguobo@gmail.com>
2019-02-11 14:25:58 +0800

    modified word doc

--- RNotes/NCP_dom.R
+++ RNotes/NCP_dom.R
@@ -1,9 +1,9 @@
 RP=500
-n1=500
-n2=500
+n1=300
+n2=700
 N=n1+n2
-f1=0.8
-f2=0.2
+f1=0.4
+f2=0.6
 para=matrix(0, RP, 6)
 paraA=matrix(0, RP, 6)
 for(i in 1:RP) {
@@ -30,14 +30,14 @@ for(i in 1:RP) {
   modA=lm(ys~Ga)
   paraA[i,5]=summary(modA)$coefficients[2,3]^2
 }
-layout(matrix(1:2, 1, 2))
-vF2=f1*(1-f1)*n1/N+f2*(1-f2)*n2/N
-ncpD=n1*n2/N * (2*f1*(1-f1)-2*f2*(1-f2))^2/vF2
-qqplot(main="Dom", rchisq(RP,1, ncp = ncpD), para[,5], pch=16, cex=0.5, bty='n',
-       xlab=expression(paste("Theoretical ", chi[1]^2)), ylab=expression(paste("Obs ",chi[1]^2)))
-abline(a=0, b=1)
+#layout(matrix(1:2, 1, 2))
+#vF2=f1*(1-f1)*n1/N+f2*(1-f2)*n2/N
+#ncpD=n1*n2/N * (2*f1*(1-f1)-2*f2*(1-f2))^2/vF2
+#qqplot(main="Dom", rchisq(RP,1, ncp = ncpD), para[,5], pch=16, cex=0.5, bty='n',
+#       xlab=expression(paste("Theoretical ", chi[1]^2)), ylab=expression(paste("Obs ",chi[1]^2)))
+#abline(a=0, b=1)
 
 ncpA=4*N*n1/N*n2/N*(mean(g1)/2-mean(g2)/2)^2/(2*mean(Ga)/2*(1-mean(Ga)/2))
-qqplot(main="Add", rchisq(RP,1, ncp = ncpA), paraA[,5], pch=16, cex=0.5, bty='n',
-       xlab=expression(paste("Theoretical ", chi[1]^2)), ylab=expression(paste("Obs ",chi[1]^2)))
-abline(a=0, b=1)
+qqplot(main="", rchisq(RP,1, ncp = ncpA), paraA[,5], pch=16, cex=0.5, bty='n',
+       xlab=expression(paste("Theoretical ", chi[1]^2)), ylab=expression(paste("Observed ",chi[1]^2)))
+abline(a=0, b=1, lty=2, col="grey")
